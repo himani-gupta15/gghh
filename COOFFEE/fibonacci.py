2f71eeb897d6b8ebbc13ef693ccb8c251de40b75
himani-gupta15 <41104884+himani-gupta15@users.noreply.github.com>
2022-04-02 10:05:38 +0530

    Add files via upload

---
+++ COOFFEE/fibonacci.py
@@ -0,0 +1,16 @@
+def recursive_fibonacci(n): 
+   if n <= 1: 
+       return n 
+   else: 
+       return(recursive_fibonacci(n-1) + recursive_fibonacci(n-2)) 
+   
+n_terms = int(input("Enter the no. :"))
+st=[]   
+# check if the number of terms is valid 
+if n_terms <= 0: 
+   print("Invalid input ! Please input a positive value") 
+else: 
+   print("Fibonacci series:") 
+   for i in range(n_terms): 
+       st.append(recursive_fibonacci(i))
+   print(st) 
